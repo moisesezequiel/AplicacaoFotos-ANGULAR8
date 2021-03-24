3895daec97fdeb57bac7d150f5c2c0287e42b38a
moisesezequiel <contato.moisesezequiel@outlook.com>
2021-03-23 22:21:50 -0300

    Criando componente de Pagina Web NotFound

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotosFormComponent } from "./photos/photos-form/photos-form.component";
 import { PhotosListComponent } from "./photos/photos-list/photos-list.component";
 
 const routes : Routes = [
   {path: 'user/flavio' , component: PhotosListComponent},
-  {path: 'p/add' , component: PhotosFormComponent}
+  {path: 'p/add' , component: PhotosFormComponent},
+  {path: '**' , component: NotFoundComponent},
 
 ];
 @NgModule({
